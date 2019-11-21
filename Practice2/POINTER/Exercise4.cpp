165ca93715dac8428d1e1ab37e5cf563bff722f6
Nguyen Van Linh <you@example.com>
2019-11-21 15:28:11 +0700

    *fix a litle problem

--- Practice2/POINTER/Exercise4.cpp
+++ Practice2/POINTER/Exercise4.cpp
@@ -5,7 +5,7 @@ int main(){
     int iVar = 10;
     int *p1 = &iVar;
     int **p2 = &p1;
-    cout<<*p1<<endl; //giá trị của ô nhớ mà p1 trỏ tới
-    cout<<*p2<<endl; //== địa chỉ của iVar;
-    cout<<*(*p2)<<endl;//==*p1==*(&iVar) == với giá trị của iVar 
+    cout<<*p1<<endl; //==10 giá trị của ô nhớ mà p1 trỏ tới 
+    cout<<*p2<<endl; //== 0x100 địa chỉ của iVar;
+    cout<<*(*p2)<<endl;//==10==*p1==*(&iVar) == với giá trị của iVar 
 }
