ffd8813f0cb0c215915bb8d5609d6a7500352513
Nguyen Van Linh <you@example.com>
2019-11-21 21:55:41 +0700

    (+) Add function input the student infor

---
+++ Practice2/StudentManagement/StudentManagement/StudentManagement.cpp
@@ -0,0 +1,71 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<vector>
+#include<iostream>
+#include<stdio.h>
+#include<string>
+using namespace std;
+
+struct Student {
+	int id;
+	string name;
+	float score;
+};
+
+typedef vector<Student> listStudent;
+
+
+int InputNumberStudent() {
+	cout << "Input number of customers (n>=1): \t";
+	int n; cin >> n;
+	while (!cin || n <= 0) {
+		cout << "Invalid number, please enter again!: \t";
+		cin.clear();
+		cin >> n;
+	}
+	return n;
+}
+void InputStudentsInfor(listStudent* vlist, Student *student) {
+
+	cout << "______________Input the student information______________" << endl;
+	int n = InputNumberStudent();
+	for (int i = 0; i < n; i++) {
+		cout << "Input the student information " << i + 1 << ":" << endl;
+		cout << "ID: "; cin >> student->id;
+		cout << "Name: "; cin.ignore(); getline(cin, student->name);
+		cout << "Score: "; cin >> student->score;
+		while (!cin || student->score < 0 || student->score>10) {
+			cout << "Invalid score, please enter again!\n";
+			cin.clear();
+			cout << "Score: "; cin >> student->score;
+		}
+		vlist->push_back(*student);
+	}
+}
+
+int main() {
+	int choice;
+	Student *student = new Student;
+	listStudent vlist;
+	int exitNumber;
+	while (exitNumber==0)
+	{
+		cout << "______________MENU______________" << endl;
+		cout << "\t1. Input" << endl;
+		cout << "\t2. Display" << endl;
+		cout << "\t3. Save to file" << endl;
+		cout << "\t4. Load from file" << endl;
+		cout << "\t0. Exit" << endl;
+		cout << "_________________________________" << endl;
+		cout << "Choose: ";
+		cin >> choice;
+		switch (choice) {
+		case 1:
+			InputStudentsInfor(&vlist, student);
+			break;
+		}
+	}
+	system("pause");
+}
