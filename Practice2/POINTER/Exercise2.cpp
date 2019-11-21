2b72c9d76f5f1453fa89c25196873f0e2638725e
Nguyen Van Linh <you@example.com>
2019-11-21 12:59:33 +0700

    fixed ex2

--- Practice2/POINTER/Exercise2.cpp
+++ Practice2/POINTER/Exercise2.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
-void main()
-{
-int a[4] = {1, 2, 3, 4};
-int *p = &a;
-int *p2 = new int;
-delete p;
-// delete[] a; k cần phải xóa vì k phải con trỏ
-delete p2;
-p = NULL; //sau khi giải phóng vùng nhớ chúng ta nên gán null tránh sữ dụng tiếp vùng nhớ đã được giải phóng mà p vẫn còn trỏ tới.
-p2 = NULL;
+int main() {
+    int a[4] = {1, 2, 3, 4};
+    int *p = a;
+    int *p2 = new int;
+    delete p;
+    // delete[] a; k cần phải xóa vì k phải con trỏ
+    delete p2;
+    p = NULL; //sau khi giải phóng vùng nhớ chúng ta nên gán null tránh sữ dụng tiếp vùng nhớ đã được giải phóng mà p vẫn còn trỏ tới.
+    p2 = NULL;
+    return 0;
 }
