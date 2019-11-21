3ec435bb74990899025aafd1eb90fee7a5a20600
Nguyen Van Linh <you@example.com>
2019-11-21 12:56:47 +0700

    * fix ex

--- Practice2/POINTER/Exercise2.cpp
+++ Practice2/POINTER/Exercise2.cpp
@@ -3,15 +3,11 @@ using namespace std;
 void main()
 {
 int a[4] = {1, 2, 3, 4};
-int *p = a;
+int *p = &a;
 int *p2 = new int;
 delete p;
-delete[] a; // Xóa mảng thì thêm []
+// delete[] a; k cần phải xóa vì k phải con trỏ
 delete p2;
 p = NULL; //sau khi giải phóng vùng nhớ chúng ta nên gán null tránh sữ dụng tiếp vùng nhớ đã được giải phóng mà p vẫn còn trỏ tới.
 p2 = NULL;
-// để giải phóng mảng a[] sau khi xóa thì có thể gán các phần từ bằng null
-for(int i = 0; i <4; i++){
-    a[i]=NULL;
-}
 }
